62a8a14f02a60d74c043430a994e0e9d342dff57
revanatha <revanathaa@gmail.com>
2025-10-08 13:34:22 +0700

    ProjectKelompok.cpp

---
+++ PenghitunganKasirSembako_ProjectKelompok.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+string produk(string pilihan) {
+    string sembako[] = {""}
+}
+int main(){
+    cout << " =================================================================================== " << endl;
+    cout << "                                   SELAMAT DATANG DI                                 " << endl;
+    cout << "                                     TOKO SEMBAKO                                    " << endl;
+    cout << " =================================================================================== " << endl;
+
+    cout << " DAFTAR HARGA SEMBAKO  HASIL INDUSTRI : " << endl << endl ;
+
+    cout << " Beras Premium               : Rp17000/kg " << endl;
+    cout << " Beras Bulog                 : Rp12000/kg " << endl;
+    cout << " Minyak Goreng Bimoli        : Rp22600/liter " << endl;
+    cout << " Minyak Goreng KITA          : Rp15000/liter " << endl;
+    cout << " Gula Pasir                  : Rp18000/kg " << endl;
+    cout << " Tepung terigu               : Rp12000/kg " << endl;
+    cout << " Tepung Terigu Segitiga Biru : Rp14000/kg " << endl;
+    cout << " Garam Kasar                 : Rp4500/bungkus " << endl;
+    cout << " Garam Halus                 : Rp3000/bungkus " << endl;
+    cout << " Susu Indomilk               : Rp4000/sachet " << endl ;
+    cout << " Blue Band Margarine         : Rp15000/sachet " << endl;
+    cout << " Telur                       : Rp58000/piring " << endl;
+    cout << " Daging Ayam                 : Rp48000/kg " << endl;
+    cout << " Daging Sapi                 : Rp150000/kg " << endl;
+    cout << " Cabe Merah                  : Rp45500/kg " << endl;
+    cout << " Cabe Merah Keriting         : Rp43000/kg " << endl;
+    cout << " Cabe Rawit Merah            : Rp45000/kg " << endl;
+    cout << " Cabe Rawit Hijau            : Rp40000/kg " << endl;
+    cout << " Cabe Rawit Hijau            : Rp40000/kg " << endl;
+
+
+
+}
