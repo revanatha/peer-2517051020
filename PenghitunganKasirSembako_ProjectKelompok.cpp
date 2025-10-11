e142346befcef318ce83a8ac3069f01526aa6dc6
Talitha Reva Nabila <revanathaa@gmail.com>
2025-10-11 07:17:01 +0700

    Update PenghitunganKasirSembako_ProjectKelompok.cpp

--- PenghitunganKasirSembako_ProjectKelompok.cpp
+++ PenghitunganKasirSembako_ProjectKelompok.cpp
@@ -12,10 +12,6 @@ int beli(int jumlah_beli) {
     int harga[19] = {17000, 12000, 22000, 15000, 18000, 12000, 14000, 4500, 3000, 15000,
                     58000, 48000, 150000, 45500, 43000, 45000, 40000, 35000, 20000};
 
-    int jumlah_beli;
-    cout << "Jumlah produk yang anda beli : ";
-    cin >> jumlah_beli;
-
     int nomor = sizeof (sembako)/sizeof(sembako[0]);
 
     int pilihan;
@@ -23,22 +19,25 @@ int beli(int jumlah_beli) {
 
     int total_harga = 0;
 
-    for(int i = 1; i <= jumlah_beli; i++){
+    for (int i = 1; i <= jumlah_beli; i++){
         cin >> pilihan;
         cout << endl;
 
-        for(int j = 0; j < nomor; j++){
-            if(j == pilihan){
+        for (int j = 0; j < nomor; j++){
+            if (j == pilihan){
                 cout  << sembako[j]<< " : Rp" << harga[j];
 
                 total_harga += harga[j];
-     }
+             }
     
         }
     }
     cout << endl;
+    
     cout << "===================================================================================" << endl;
+    
     cout << "Total harga produk yang anda beli : Rp " << total_harga << endl;
+    
     cout << "===================================================================================" << endl;
 
     int jumlah_uang;
@@ -51,12 +50,12 @@ int beli(int jumlah_beli) {
 }
 
 int main(){
-    cout << "===================================================================================" << endl;
-    cout << "                                   SELAMAT DATANG DI                               " << endl;
-    cout << "                                     TOKO SEMBAKO                                  " << endl;
-    cout << "===================================================================================" << endl;
+    cout << "====================================================================================" << endl;
+    cout << "                                   SELAMAT DATANG DI                                " << endl;
+    cout << "                                     TOKO SEMBAKO                                   " << endl;
+    cout << "====================================================================================" << endl;
     cout << "|  KODE          |          DAFTAR HARGA SEMBAKO  HASIL INDUSTRI                   |" << endl;
-    cout << "=================================================================================== " << endl;
+    cout << "====================================================================================" << endl;
     cout << "|   [Kode 0]     |      Beras Premium                     : Rp17000/kg             |" << endl;
     cout << "|   [Kode 1]     |      Beras Bulog                       : Rp12000/kg             |" << endl; 
     cout << "|   [Kode 2]     |      Minyak Goreng Bimoli              : Rp22600/liter          |" << endl;
@@ -77,8 +76,7 @@ int main(){
     cout << "|   [Kode 17]    |      Cabe Rawit Hijau                  : Rp40000/kg             |" << endl;
     cout << "|   [Kode 18]    |      Bawang Putih                      : Rp35000/kg             |" << endl;
     cout << "|   [Kode 19]    |      Bawang Merah                      : Rp20000/kg             |" << endl;
-    cout << "====================================================================================" <<endl;
-    
+    cout << "====================================================================================" << endl;
     
     int jumlah_beli;
     cout << "Jumlah produk yang anda beli : ";
